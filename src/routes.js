9203fb50bba8dfeb8cea84f209f42bfa67fcdcc6
hayden <haydenkjb@me.com>
2019-07-30 13:28:09 -0400

    Added routes.js
    Idea is to use markdown files to render out page__content,

---
+++ src/routes.js
@@ -0,0 +1,19 @@
+const Home = {
+  path: '/',
+  header: 'hello',
+  content: '/pages/home.md',
+};
+
+const About = {
+  path: '/',
+  header: 'about',
+  content: '/pages/about.md',  
+};
+
+const pageArray = [Home, About];
+const pageObject = {Home, About};
+
+export default {
+  pageArray,
+  pageObject,
+},
