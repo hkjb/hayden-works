88568263631d0e7e63314e78ac97fd7725e8c932
hayden <haydenkjb@me.com>
2019-07-31 14:52:46 -0400

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
+import HomePage from 'pages/home.md';
+import AboutPage from 'pages/about.md';
+
 const Home = {
   path: '/',
   header: 'hello',
-  content: '/pages/home.md',
+  content: HomePage,
 };
 
 const About = {
   path: '/about',
   header: 'about',
-  content: '/pages/about.md',  
+  content: AboutPage,  
 };
 
 const pageArray = [Home, About];
