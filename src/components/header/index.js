d9a30bc4c48fd05e08f1163b4c4af2e963911a46
hayden <haydenkjb@me.com>
2019-08-01 11:28:16 -0400

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import Routes from 'routes';
 
 import './styles.scss';
@@ -7,16 +7,16 @@ import './styles.scss';
 function Header() {
 
   const links = Routes.pageArray.map((route, i) => (
-    <Link key={i} to={route.path} className="app__header__link">{route.header}</Link>
+    <NavLink key={i} to={route.path} className="app__header__link">{route.header}</NavLink>
   ));
   
   return (
     <header className="app__header">
-      <Link to="/" className="app__header__home">
+      <NavLink to="/" className="app__header__home">
         <span className="icon">
           <i className="fa fa-rabbit-fast" />
         </span>
-      </Link>
+      </NavLink>
       {links}
     </header>
   );
