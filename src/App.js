cb697875f536bd426233bb4324468998a27d78fb
hayden <haydenkjb@me.com>
2019-08-01 11:38:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import 'App.scss';
 function App() {
   
   const pages = Routes.pageArray.map((route, i) => (
-    <Route key={i} path={route.path} component={() => <MarkdownReader src={route.content} />} />
+    <Route exact key={i} path={route.path} component={() => <MarkdownReader src={route.content} />} />
   ));
 
   return (
