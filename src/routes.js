2fb2390b5eae1f9c82193054927a9ae80ace5bdc
hayden <haydenkjb@me.com>
2020-01-20 18:29:39 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import Best2018 from 'pages/2018.md';
 // Page Object Parameters
 // path:      link appended to hayden.works
 // header:    determines the page and nav title
+// icon:      fontawesome icon for mobile display
 // content:   attached markdown for display
 // show:      determines visibility on all navs
 // children:  only applicable for top level sections, includes additional page objects
@@ -14,17 +15,20 @@ const pageObject = {
   Home: {
     path: '/',
     header: 'home',
+    icon: 'house',
     content: HomePage,
   },
   Contact: {
     path: '/contact',
     header: 'contact',
+    icon: 'phone',
     content: 'Reach me at: --- --- ----',
     show: false,
   },
   BestOf: {
     path: '/best-of',
     header: 'best of',
+    icon: 'star',
     children: [
       {
         path: '/2019',
