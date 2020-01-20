c43d7e8764ed172462d40a48aa404a693c8b74d6
hayden <haydenkjb@me.com>
2020-01-20 14:33:41 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,30 @@ import HomePage from 'pages/home.md';
 import Best2019 from 'pages/2019.md';
 import Best2018 from 'pages/2018.md';
 
+// Page Object Parameters
+// highlight: determines visibility on main content nav
+// path:      link appended to hayden.works
+// header:    determines the page and nav title
+// content:   attached markdown for display
+// show:      determines visibility on all navs
+// children:  only applicable for top level sections, includes additional page objects
+
 const pageObject = { 
   Home: {
+    highlight: true,
     path: '/',
     header: 'home',
     content: HomePage,
   },
   Contact: {
+    highlight: true,
     path: '/contact',
     header: 'contact',
     content: 'Reach me at: --- --- ----',
     show: false,
   },
   BestOf: {
+    highlight: false,
     path: '/best-of',
     header: 'best of',
     children: [
