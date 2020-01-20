288ab02ea7eb941a033f5bd2612b17e683ccead8
hayden <haydenkjb@me.com>
2020-01-20 14:38:35 -0500

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,30 +5,43 @@ import Routes from 'routes';
 import './styles.scss';
 
 function Header() {
+  
+  const showMenu = false;
 
   const icons = ['computer-classic'];
   const icon = icons[Math.floor(Math.random() * icons.length)];
   
+  toggleMenu() {
+    showMenu = !showMenu;
+  }
+  
   return (
-    <header className="app__header">
-      <NavLink to="/" className="app__header__home">
-        <i className={`fa fa-${icon}`} />
-      </NavLink>
-      
-      { Routes.pageArray.map((route, i) => route.children ? (
-          <React.Fragment key={i}>
-            <NavLink to={route.path} className="app__header__link">{route.header}</NavLink>
-            <div className="app__header__children">
-              { route.children.map((child, i) => child.show !== false && (
-                <NavLink key={i} to={`${route.path}${child.path}`} className="app__header__link">{child.header}</NavLink>
-              ))}
-            </div>
-          </React.Fragment>
-        ) : route.show !== false && (
+    <>
+      <div className={`app__drawer ${showMenu && 'app__drawer--open'}`}>
+        { Routes.pageArray.map((route, i) => route.show !== false && (
           <NavLink key={i} to={route.path} className="app__header__link">{route.header}</NavLink>
-        )
-      )}
-    </header>
+        )}
+      </div>
+      <header className="app__header">
+        <button className="app__header__home" onClick={this.toggleMenu}>
+          <i className={`fa fa-${icon}`} />
+        </button>
+
+        { Routes.pageArray.map((route, i) => route.highlight && route.children ? (
+            <React.Fragment key={i}>
+              <NavLink to={route.path} className="app__header__link">{route.header}</NavLink>
+              <div className="app__header__children">
+                { route.children.map((child, i) => child.show !== false && (
+                  <NavLink key={i} to={`${route.path}${child.path}`} className="app__header__link">{child.header}</NavLink>
+                ))}
+              </div>
+            </React.Fragment>
+          ) : route.show !== false && (
+            <NavLink key={i} to={route.path} className="app__header__link">{route.header}</NavLink>
+          )
+        )}
+      </header>
+    </>
   );
 }
 
