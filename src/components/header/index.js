a1d7ab91c371815b738eb03559225b278905b051
hayden <haydenkjb@me.com>
2020-03-04 12:01:11 -0500

    Update index.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -37,7 +37,7 @@ function Header() {
         <a className="app__navigation__link" href="https://github.com/hkjb" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github" />
         </a>
-        <a className="app__navigation__link" href="https://twitter.com/hhhkkkjjjbbb" target="_blank" rel="noopener noreferrer">
+        <a className="app__navigation__link" href="https://twitter.com/teevee_boy" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-twitter" />
         </a>
         <a className="app__navigation__link" href="https://stackoverflow.com/users/4953804" target="_blank" rel="noopener noreferrer">
