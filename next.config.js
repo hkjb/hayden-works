6d0bbec246b61899e94bd00a29c2312469e04603
hayden bailey <hkjb@hey.com>
2023-06-07 03:55:50 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,11 +15,11 @@ const withMDX = require('@next/mdx')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: export,
     // Configure pageExtensions to include md and mdx
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     // Optionally, add any other Next.js config below
-    reactStrictMode: true,
-    distDir: 'dist'
+    reactStrictMode: true
 };
 
 // Merge MDX config with Next.js config
