80e4d3d50c73850a10fa9e2ce557f54a6d08b31c
Hayden Bailey <hkjb@hey.com>
2025-12-15 23:12:48 -0500

    Cleanup

--- next.config.js
+++ next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
     // Configure pageExtensions to include md and mdx
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     // Optionally, add any other Next.js config below
-    reactStrictMode: true
+    reactStrictMode: true,
+    output: "export"
 };
 
 // Merge MDX config with Next.js config
